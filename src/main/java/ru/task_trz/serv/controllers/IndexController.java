f049eed3c9ea102aecb263c76de14a41b0179f10
imarochkin <imarochkin@octopus.name>
2021-09-25 18:28:03 +0300

    Change dirs structure

---
+++ src/main/java/ru/task_trz/serv/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ru.task_trz.serv.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String checkIndex() {
+        return "<h1>Task-trz-serv</h1>\r\n<p>Version: " + getClass().getPackage().getImplementationVersion() + "</p>\r\n";
+    }
+}
